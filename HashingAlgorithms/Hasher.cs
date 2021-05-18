155328adad86b4ca201e2b15c41749e20ad6e397
Ivan <Ivan@DESKTOP-LCTUROF>
2021-05-18 16:24:59 +0200

    Add project files.

---
+++ HashingAlgorithms/Hasher.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace HashingAlgorithms
+{
+    public static class Hasher
+    {
+        private static string GenerateHashString(HashAlgorithm algorithm, string text)
+        {
+            var bytes = Encoding.UTF8.GetBytes(text);
+            algorithm.ComputeHash(bytes);
+
+            var hash = algorithm.Hash;
+
+            var result = string.Join(string.Empty, hash.Select(x => x.ToString("x2")));
+            return result;
+        }
+
+        public static string MD5(string text)
+        {
+            var result = default(string);
+
+            using (var algorithm = new MD5CryptoServiceProvider())
+            {
+                result = GenerateHashString(algorithm, text);
+            }
+
+            return result;
+        }
+
+        public static string SHA1(string text)
+        {
+            var result = default(string);
+
+            using (var algorithm = new SHA1Managed())
+            {
+                result = GenerateHashString(algorithm, text);
+            }
+
+            return result;
+        }
+
+        public static string SHA256(string text)
+        {
+            var result = default(string);
+
+            using (var algorithm = new SHA256Managed())
+            {
+                result = GenerateHashString(algorithm, text);
+            }
+
+            return result;
+        }
+
+        public static string SHA512(string text)
+        {
+            var result = default(string);
+
+            using (var algorithm = new SHA512Managed())
+            {
+                result = GenerateHashString(algorithm, text);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HashingAlgorithms/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! \n\nEnter the value you want to hash: ");
+            string inputValue = Console.ReadLine();
+
+            //GetSingleAlgorithmValue(inputValue);
+            GetAllAlgorithmValues(inputValue);
+
+            Console.ReadKey();
+        }
+
+        static void GetAllAlgorithmValues(string inputValue)
+        {
+            foreach (var algorithm in GetAlgorithms())
+            {
+                string hash = algorithm.Value.Invoke(inputValue);
+                PrintValue(algorithm.Key, hash);
+            }
+        }
+
+        static void GetSingleAlgorithmValue(string inputValue)
+        {
+            var algorithms = GetAlgorithms();
+
+            Console.WriteLine("\nPlease enter the algorithm name: MD5 | SHA1 | SHA256 | SHA512");
+            string algorithmName = Console.ReadLine().ToUpper();
+
+            while (!algorithms.ContainsKey(algorithmName))
+            {
+                Console.WriteLine("\nEnter a valid algorithm name: ");
+                algorithmName = Console.ReadLine().ToUpper();
+            }
+
+            string hash = algorithms[algorithmName].Invoke(inputValue);
+            PrintValue(algorithmName, hash);
+        }
+
+        static void PrintValue(string algorithmName, string hash)
+        {
+            Console.Write($"\nHash value ({algorithmName}):\n{hash} \n");
+            Console.Write($"- Length: {hash.Length}\n");
+        }
+
+        static Dictionary<string, Func<string, string>> GetAlgorithms()
+        {
+            return new Dictionary<string, Func<string, string>>
+            {
+                { nameof(Hasher.MD5).ToUpper(), Hasher.MD5 },
+                { nameof(Hasher.SHA1).ToUpper(), Hasher.SHA1 },
+                { nameof(Hasher.SHA256).ToUpper(), Hasher.SHA256 },
+                { nameof(Hasher.SHA512).ToUpper(), Hasher.SHA512 }
+            };
+        }
+    }
+}
